bb3ac7de25f1190de86157a90d387794f9543568
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-03 15:00:28 +0000

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -16,7 +16,14 @@ class PetsPage extends Component {
     return (
       <div>
         <h1>Pets Page</h1>
-        <PetsList pets={this.props.fetchPets()} />
+        <PetsList pets={this.props.pets} />
+        <Switch>
+        <Route path="/pets/new" component={PetsNew} />
+       <Route path="/pets/:petId" component={PetsShow}/>
+       <Route exact path="/pets" render={() => (
+         <h3>Please select a Pet from the list.</h3>
+       )}/>
+        </Switch>
       </div>
       )
   }
